040e2767a9c9964e9469e501cbbf28639b8e9c8e
root <ybgyay@gmail.com>
2022-12-13 08:23:55 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <iostream>
-using std::cout;
+#include <unistd.h>
+#include "main.h"
 
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line
+ * Return: Always 0
+ */
 int main(void)
 {
+	char word[8] = "_putchar";
+	int i;
 
-	std::cout << "_putchar\n";
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
 
 	return (0);
-
 }
