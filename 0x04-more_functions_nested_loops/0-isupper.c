df0817102257153fd448147b5c08dd7485a77180
root <ybgyay@gmail.com>
2022-12-15 14:29:54 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - Check for uppercase
- * @x: The letter to be checked
+ * @c: The letter to be checked
  * Return: 1 for uppercase, 0 for anything else
  */
 
-int _isupper(int x)
+int _isupper(int c)
 
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 	return (0);
 	}
