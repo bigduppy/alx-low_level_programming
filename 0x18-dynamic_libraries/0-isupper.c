23ac9f366ad42f1a9ba4a00baaf5c7e6b57b19da
bigduppy <ybgyay@gmail.com>
2023-03-24 19:31:05 -0700

    TASK 0

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase
+ * @c: The letter to be checked
+ * Return: 1 for uppercase, 0 for anything else
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (0);
+	}
+	return (1);
+}
---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies inputted number of bytes from string src to dest.
+ * @dest: buffer storing the string copy
+ * @src: source string
+ * @n: max number of bytes to be copied from src
+ * Return: a pointer to resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+	dest[index] = '\0';
+
+	return (dest);
+
+}
